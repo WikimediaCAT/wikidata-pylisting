408587077602caad00d0fb505c91ebb5b88c3e31
Toni Hermoso Pulido <toniher@cau.cat>
2019-09-28 17:29:50 +0200

    update pandas

--- darreresBios.py
+++ darreresBios.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
+import numpy as np
 import io
+from urllib.parse import unquote
 
 # Import JSON configuration
 
@@ -23,4 +25,14 @@ response = requests.get('https://query.wikidata.org/sparql', headers=headers, pa
 
 c=pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 
+c['article'] = c['article'].apply(lambda x: unquote( x.replace("https://ca.wikipedia.org/wiki/", "") ) )
+c['item'] = c['item'].apply( lambda x: x.replace("http://www.wikidata.org/entity/", "") )
+
+creation_date = np.zeros( ( len(c) ), dtype='datetime64' )
+
+for g, df in c.groupby(np.arange(len(c)) // 50):
+	titles = df['article'].str.cat(sep="|")
+
+
 print( c )
+
