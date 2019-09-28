3304f3f04955068a6ff2e42cadbe50af8d89cbab
Toni Hermoso Pulido <toniher@cau.cat>
2019-09-28 12:35:05 +0200

    back

--- darreresBios.py
+++ darreresBios.py
@@ -1,17 +1,23 @@
-from SPARQLWrapper import SPARQLWrapper, TSV
-
-
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql?query=")
-sparql.setQuery("""
+query = """
 SELECT ?item ?itemLabel 
 WHERE 
 {
   ?item wdt:P31 wd:Q146.
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+} limit 10
+"""
+
+
+import requests
+import pandas as pd
+import io
+
+headers = {
+    'Accept': 'text/tab-separated-values',
 }
-""")
-sparql.setReturnFormat(TSV)
-results = sparql.query().convert()
+params = { 'query':  query }
+response = requests.get('https://query.wikidata.org/sparql', headers=headers, params=params)
 
+c=pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 
-print( results )
+print( c )
