0b00e213bd64b39233c982ad20e0667f226e7680
Toni Hermoso Pulido <toniher@cau.cat>
2019-09-28 15:10:32 +0200

    update csv

--- darreresBios.py
+++ darreresBios.py
@@ -5,16 +5,17 @@ import io
 # Import JSON configuration
 
 query = """
-SELECT ?item ?itemLabel 
-WHERE 
-{
-  ?item wdt:P31 wd:Q146.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-} limit 10
+SELECT ?item ?itemLabel ?article WHERE {
+  ?item wdt:P31 wd:Q5.
+  ?item wdt:P21 wd:Q6581072 .
+  ?article schema:about ?item .
+  ?article schema:isPartOf <https://ca.wikipedia.org/> .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],ca" } .
+} ORDER BY ?article
 """
 
 headers = {
-	'Accept': 'text/tab-separated-values',
+	'Accept': 'text/csv',
 	'User-Agent': 'darreresBio/0.1.0 (https://github.com/WikimediaCAT/wikidata-pylisting; toniher@wikimedia.cat) Python/3.7',
 }
 params = { 'query':  query }
